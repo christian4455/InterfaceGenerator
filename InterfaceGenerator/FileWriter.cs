948b76e142b9f3ea7bb795ada65a3c0c56237523
Christian Behnisch <c.behnisch@gmx.net>
2016-12-04 21:27:10 +0100

    added missing classes

---
+++ InterfaceGenerator/FileWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using InterfaceGenerator.Types;
+using InterfaceGenerator.Utils.Logger;
+
+namespace InterfaceGenerator
+{
+    class FileWriter : IFileWriter
+    {
+        public FileWriter()
+        {
+            /* Intentionally left blank */
+        }
+
+        public void WriteProduct(string path, Product product)
+        {
+            Log.Info("");
+            try
+            {
+                System.IO.StreamWriter file = new System.IO.StreamWriter(path + product.GetFilename());
+                file.WriteLine(product.GetProduct().ToString());
+                file.Close();
+            }
+            catch (Exception e)
+            {
+                Log.Info("Exception:" + e.Message);
+            }
+            finally
+            {
+                // nothing
+            }
+        }
+    }
+}
---
+++ InterfaceGenerator/InterfaceGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InterfaceGenerator.Types;
+
+namespace InterfaceGenerator
+{
+    public class InterfaceGenerator : IPlugin
+    {
+        IRepositoryHandler m_RepositoryHandler = null;
+        IInterfaceBuilder m_InterfaceBuilder = null;
+        IFileWriter m_IFileWriter = null;
+
+        public InterfaceGenerator()
+        {
+            /* Intentionally left blank */
+        }
+        
+        void IPlugin.ProcessRepository(EA.Repository repository)
+        {
+            InterfaceData data = m_RepositoryHandler.HandleRepository(repository);
+        }
+
+        public void SetRepositoryHandler(IRepositoryHandler repositoryHandler)
+        {
+            m_RepositoryHandler = repositoryHandler;
+        }
+
+        public void SetInterfaceBuilder(IInterfaceBuilder interfaceBuilder)
+        {
+            m_InterfaceBuilder = interfaceBuilder;
+        }
+
+        public void SetFileWriter(IFileWriter iFileWriter)
+        {
+            m_IFileWriter = iFileWriter;
+        }
+    }
+}
